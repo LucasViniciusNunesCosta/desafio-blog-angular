3a4c7775de1664e31a2385805626a0eb979fc977
Lucas Costa <lucasvinicius3105@gmail.com>
2022-12-18 23:02:25 -0300

    content page dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        'id': '1',
+        'photoCover': "https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+        'title': 'Windows 11 - Opnião dos usuários',
+        'description': 'O sistema operacional da Microsoft foi lançado no ano de 2022 e nem todos os usuários desejam migrar s istema operacional da maça realmente ajuda a ser mais produtivo se comparado ao windows?'
+    },
+    {
+        'id': '2',
+        'photoCover': "https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+        'title': 'Design criativo',
+        'description': 'O design que além de agradável é funcional'
+    }
+]
